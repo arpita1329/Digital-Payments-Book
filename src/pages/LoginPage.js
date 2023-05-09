ea1f09bfcf63998c2f60afdb2fee84d860bb1b3c
Aryanxfa <aryan200152@gmail.com>
2023-05-09 18:15:10 +0530

    refactor code

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -78,9 +78,7 @@ export default function LoginPage() {
 
             <Typography variant="body2" sx={{ mb: 5 }}>
               Don’t have an account? {''}
-              <Link variant="subtitle2" component={RouterLink} to={register}>
-                Get started
-              </Link>
+              <Link variant="subtitle2" component={RouterLink} to={register}>Get started</Link>
             </Typography>
 
             <Stack direction="row" spacing={2}>
