92ee79260d8c579779ff27a9e0bdca428b3c550c
Aryanxfa <aryan200152@gmail.com>
2023-05-09 11:21:26 +0530

    server: lint and change vars

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 try {
   const conn = mongoose.connect(process.env.MONGO_URI, {
@@ -18,7 +18,7 @@ try {
   });
   console.log(`Mongodb connected successfully`);
 } catch (e) {
-  console.log("Mongodb connection failed.", e);
+  console.log('Mongodb connection failed.', e);
 }
 
 const UserSchema = mongoose.Schema({
@@ -28,47 +28,47 @@ const UserSchema = mongoose.Schema({
   email: String,
 });
 
-const UserModel = mongoose.model("user", UserSchema);
+const UserModel = mongoose.model('user', UserSchema);
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  UserModel.findOne({ username: username }, (err, user) => {
+  UserModel.findOne({ username }, (err, user) => {
     if (user) {
       if (bcrypt.compareSync(password, user.password)) {
-        res.send({ message: "Login Success", user: user });
+        res.send({ message: 'Login Success', user });
       } else {
-        res.send({ message: "Incorrect Password" });
+        res.send({ message: 'Incorrect Password' });
       }
     } else {
-      res.send("User is not registered");
+      res.send('User is not registered');
     }
   });
 });
 
-app.post("/register", (req, res) => {
+app.post('/register', (req, res) => {
   const { username, email, password } = req.body;
 
-  var salt = bcrypt.genSaltSync(10);
-  var hash = bcrypt.hashSync(password, salt);
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
 
   UserModel.create(
     {
-      username: username,
-      email: email,
+      username,
+      email,
       password: hash,
     },
     (err, user) => {
       console.log(user);
       if (user) {
-        res.send({ message: "Registration Successful" });
+        res.send({ message: 'Registration Successful' });
       } else {
-        res.send({ message: "User already registered" });
+        res.send({ message: 'User already registered' });
       }
     }
   );
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server Started");
+  console.log('Server Started');
 });
